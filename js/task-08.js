008d30b095697de4f891648dcef02d8bf27e691c
Molksy <mikefromenclave@gmail.com>
2023-04-30 18:15:23 +0300

    Update task-08.js
    +add alert if email includes "ru"

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // предотвращаем перезагрузку страницы
+
+  const { email, password } = event.target.elements; // получаем доступ к элементам формы
+
+  if (email.value === '' || password.value === '') { // проверяем, заполнены ли все поля
+    alert('All fields must be filled!');
+    return; // прерываем выполнение функции, если есть незаполненные поля
+  }
+
+  if (email.value.includes('.ru')) {
+    alert('Русский военный корабль иди на ***!');
+    return; // прерываем выполнение функции, если есть незаполненные поля
+  }
+
+  const userData = { // создаем объект с данными пользователя
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(userData); // выводим объект с данными в консоль
+  form.reset(); // очищаем поля формы
+});
