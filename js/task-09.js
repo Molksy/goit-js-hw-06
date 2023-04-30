82d8c906b0d0e999067ca01983a052a11a8490cb
Molksy <mikefromenclave@gmail.com>
2023-04-30 18:18:04 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const button = document.querySelector('button.change-color');
+const colorBlock = document.querySelector('span.color');
+
+button.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorBlock.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
