6065fcff8858681649425c082e04d41dfa7047bb
Molksy <mikefromenclave@gmail.com>
2023-04-30 18:46:50 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,38 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const controls = document.querySelector('#controls');
+const input = controls.querySelector('input');
+const createBtn = controls.querySelector('button[data-create]');
+const destroyBtn = controls.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i += 1) {
+    const size = 30 + i * 10;
+    const box = document.createElement('div');
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    fragment.appendChild(box);
+  }
+
+  boxes.appendChild(fragment);
 }
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+createBtn.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
