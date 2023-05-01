228c7b863ed698e614a665ee6006859460d51d71
Molksy <mikefromenclave@gmail.com>
2023-05-01 19:36:35 +0300

    fix bugs in 1 and 7 tasks

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,10 @@
-const categories = document.querySelectorAll('#categories .item');
+const categories = document.getElementById('categories').children;
 
 console.log(`Number of categories: ${categories.length}`);
 
-categories.forEach(category => {
+for (const category of categories) {
   const categoryName = category.querySelector('h2').textContent;
   const categoryElements = category.querySelectorAll('li').length;
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements}`);
-});
+}
